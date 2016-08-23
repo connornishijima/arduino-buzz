ac6a0ff482ce1f474c0f551c07c2698a452c4624
Connor Nishijima <connornishijima@gmail.com>
2016-08-22 22:17:57 -0600

    Create Buzz.h

---
+++ src/Buzz.h
@@ -0,0 +1,23 @@
+/*
+  Buzz.h - Library for detecting motion via AC electricity
+  Created by Connor Nishijima, August 22nd 2016.
+  Released under the GPLv3 license.
+*/
+#ifndef buzz_h
+#define buzz_h
+
+#include "Arduino.h"
+
+class Buzz
+{
+  public:
+    Buzz();
+    void begin(uint16_t pin, uint8_t hz, uint16_t wait);
+    void end();
+    void printData();
+    void setAlarm(void (*g)(), uint16_t thresh, uint16_t hold);
+    void checkAlarm();
+    int16_t level();
+};
+
+#endif
